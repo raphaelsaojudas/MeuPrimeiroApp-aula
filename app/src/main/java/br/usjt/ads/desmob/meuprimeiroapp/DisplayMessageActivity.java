8e86150ccb9e39df3cca642f244fe786fbf1a793
Raphael Gomes <rgomes@CACS.LOCAL>
2018-10-01 11:30:57 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ads/desmob/meuprimeiroapp/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package br.usjt.ads.desmob.meuprimeiroapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String mensagem = intent.getStringExtra(MainActivity.TEXTO);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(mensagem);
+
+        ViewGroup layout = findViewById(R.id.activit_display_message);
+        layout.addView(textView);
+    }
+}
---
+++ app/src/main/java/br/usjt/ads/desmob/meuprimeiroapp/Main2Activity.java
@@ -0,0 +1,13 @@
+package br.usjt.ads.desmob.meuprimeiroapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
